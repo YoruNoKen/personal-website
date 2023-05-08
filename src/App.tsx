c800f6fa17398eee0ac217499b2ae1db49c6d4b9
Yoru <69751661+YoruNoKen@users.noreply.github.com>
2023-05-08 21:57:32 +0300

    added more elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import pfp from "./img/pfp.png";
 import youtube_icon from "./img/Youtube.png";
 import twitter_icon from "./img/Twitter.png";
 import github_logo from "./img/GitHub.png";
+import screenshot1 from "./img/screenshot1.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="container">
+        <div className="bio-container">
           <h6
             className="Welcome"
-            rel="noopener noreferrer">
+            rel="noopener norefer rer">
             Welcome!
           </h6>
           <p className="bio">
@@ -53,10 +54,32 @@ function App() {
               <img
                 src={github_logo}
                 alt="GitHub"
-                className="icon"></img>
+                className="icon"
+              />
             </a>
           </div>
         </div>
+        <div className="video-container">
+          <p className="video-title">Here's a recent video of mine:</p>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/mVtehI3Xf6o"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+        </div>
+        <div className="github-container">
+          <p className="github-title">Some of the projects I've worked on:</p>
+          <br></br>
+          <h2 className="mia-osu">Mia-osu</h2>
+          <p className="mia-osu-text">A Discord bot written in JavaScript for osu!game</p>
+          <img
+            src={screenshot1}
+            alt="Screenshot"
+            className="image-container"
+          />
+        </div>
+        <p>pwease send me luv</p>
       </header>
     </div>
   );
